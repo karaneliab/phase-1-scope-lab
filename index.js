6005b9af35f102cd1d9cad1ff7634d1f7879f0d4
karaneliab <eliabkaran7884@gmail.com>
2023-12-14 09:17:12 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob";
+    function upperCaseCustomerName(){
+        if(customerName){
+            customerName = customerName.toUpperCase();
+            console.log("Uppercased customerName:", customerName);
+        }else{
+            console.log(undefined);
+        }
+    }
+   function setBestCustomer (){
+    const bestCustomer = 'not bob';
+    return bestCustomer;
+   }
+    
+
+    
